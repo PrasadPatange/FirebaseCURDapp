5ce8261a1e787f082b58098b9181d4e457690663
Prasad <prasadpatange2603@gmail.com>
2021-11-01 18:18:09 +0530

    config commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBSq-7ReVd39Vrtqqb8oVSc3KJJpQ8mtJU",
+    authDomain: "angularfirebasecrudapp-e704a.firebaseapp.com",
+    databaseURL: "https://angularfirebasecrudapp-e704a-default-rtdb.firebaseio.com",
+    projectId: "angularfirebasecrudapp-e704a",
+    storageBucket: "angularfirebasecrudapp-e704a.appspot.com",
+    messagingSenderId: "598726498423",
+    appId: "1:598726498423:web:d85c1625ecffc6ca614e72",
+    measurementId: "G-LXNFDWHPNE"
+  }
 };
